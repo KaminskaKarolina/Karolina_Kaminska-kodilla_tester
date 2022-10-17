260b17d38ff3356a423e5c96a9eb0298e97da0e7
Karolina Kaminska <kaminskakarolina001@gmail.com>
2022-10-17 20:38:26 +0200

    Main

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,20 @@
+public class MainExtended {
+    public static void main(String[]args){
+        String myName = "Karolina";
+        String myCity = "Katowice";
+        int thisYear = 2022;
+        int myAge = 29;
+        boolean isSummer = false;
+        char bestVitamin = 'C';
+        System.out.println(myName);
+        System.out.println(myCity);
+        System.out.println(thisYear);
+        System.out.println(myAge);
+        System.out.println(isSummer);
+        System.out.println(bestVitamin);
+
+
+
+
+    }
+}
