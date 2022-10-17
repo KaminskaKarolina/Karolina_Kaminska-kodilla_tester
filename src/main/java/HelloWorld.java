0bc126da1f71373bddfaeabe8c0871b647cf54bd
Karolina Kaminska <kaminskakarolina001@gmail.com>
2022-10-17 20:01:09 +0200

    Initial version

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("HelloWorld!");
+        System.out.println("Karolina");
+        System.out.println(29);
+        System.out.println('K');
+    }
+}
